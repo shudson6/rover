6a9d97573cd0dcf83338d1347905344dc41899a7
Steven Hudson <shudson6@asu.edu>
2021-08-14 12:20:26 -0400

    change enum CommandCode_t, add Command_clear in command.h

--- include/command.h
+++ include/command.h
@@ -8,11 +8,12 @@
 #define __INCLUDE_COMMAND_INTERPRETER
 
 typedef enum {
-  COMMAND_ERROR,
-  COMMAND_STATUS,
-  COMMAND_ENABLE,
-  COMMAND_SETDIR,
-  COMMAND_RUN
+  CMDCODE_NULL = 0,
+  CMDCODE_ERROR,
+  CMDCODE_STATUS,
+  CMDCODE_SETEN,
+  CMDCODE_SETDIR,
+  CMDCODE_RUN,
 } CommandCode_t;
 
 typedef enum {
@@ -22,21 +23,34 @@ typedef enum {
   CMDERR_UNRECOGNIZED_PARAM,
   CMDERR_UNEXPECTED_PARAM,
   CMDERR_MOTOR_EXPECTED,
-  CMDERR_TIME_EXPECTED
+  CMDERR_TIME_EXPECTED,
+
+  // too many commands in the command buffer
+  CMDERR_BUFFER_FULL
 } CommandParseError_t;
 
 typedef struct {
   CommandCode_t commandCode;
+
   // bits 0-5 indicate which drive motors are affected
   unsigned char driveMotors;
+
   // bits 0-3 indicate which steering motors are affected
   unsigned char turnMotors;
+
   // numeric parameter, e.g.:
   // duration for 'run'
   // direction for 'setdir'
   // on/off for 'enable'
   unsigned char value;
 } Command_t;
+
+/********************************************************************************
+ * void Command_clear(Command_t* cmd)
+ *
+ * Sets all members of cmd to zero.
+ */
+void Command_clear(Command_t* cmd);
 
 /********************************************************************************
  * const char* Command_getErrorMessage(CommandParseError_t errCode)
@@ -46,16 +60,16 @@ typedef struct {
 const char* Command_getErrorMessage(CommandParseError_t errCode);
 
 /********************************************************************************
- * CommandParseError_t Command_parse(const char* str, Command_t* cmd) 
+ * CommandParseError_t Command_parse(const char* str)
  *
  * Parse a string into a Command_t.
- * If str is a valid command string, it will be encoded into cmd and this
- * function will return zero.
+ * If str is a valid command string, it will be added to the command buffer and
+ * this function will return zero.
  * If parsing fails, return value will be a nonzero error code. A descriptive
  * error message can be obtained by calling Command_getErrorMessage() with the
  * error code.
  */
-CommandParseError_t Command_parse(const char* str, Command_t* cmd);
+CommandParseError_t Command_parse(const char* str);
 
 #endif // __INCLUDE_COMMAND_INTERPRETER
 
